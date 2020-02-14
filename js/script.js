b70997228d85b9f35df9879193291fdc68bad3a9
DKamman <42833740+DKamman@users.noreply.github.com>
2020-02-14 12:16:37 +0100

    Created front-end, trying to loop through array

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const image = document.getElementById("image");
+const title = document.getElementById("title");
+const description = document.getElementById("description");
+const price = document.getElementById("price");
+
+var pizzas = [
+    {name: 'Hawaii', ingrediënts: 'Tomatensaus, Kaas, Ham, Ananas', prijs: '8,-'},
+    {name: 'Borromea', ingrediënts: 'Tomatensaus, Kaas, Ham', prijs: '6,50'},
+    {name: 'Calzone', ingrediënts: 'Tomatensaus, Mozarella, Salami, Champignons, Geraspte kaas', prijs: '9,50'},
+];
+
+console.log(pizzas);
+
+console.log(image);
+        
