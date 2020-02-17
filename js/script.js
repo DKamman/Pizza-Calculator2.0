b033233596cc4b831128f1fddc3682566d39515e
DKamman <42833740+DKamman@users.noreply.github.com>
2020-02-17 16:51:57 +0100

    updated foreach

--- js/script.js
+++ js/script.js
@@ -1,15 +1,34 @@
-const image = document.getElementById("image");
-const title = document.getElementById("title");
-const description = document.getElementById("description");
-const price = document.getElementById("price");
+const table = document.getElementById('pizza-table');
 
 var pizzas = [
-    {name: 'Hawaii', ingrediënts: 'Tomatensaus, Kaas, Ham, Ananas', prijs: '8,-'},
-    {name: 'Borromea', ingrediënts: 'Tomatensaus, Kaas, Ham', prijs: '6,50'},
-    {name: 'Calzone', ingrediënts: 'Tomatensaus, Mozarella, Salami, Champignons, Geraspte kaas', prijs: '9,50'},
+    {name: 'Hawaii', ingredients: 'Tomatensaus, Kaas, Ham, Ananas', prijs: '8,-'},
+    {name: 'Borromea', ingredients: 'Tomatensaus, Kaas, Ham', prijs: '6,50'},
+    {name: 'Calzone', ingredients: 'Tomatensaus, Mozarella, Salami, Champignons, Geraspte kaas', prijs: '9,50'},
 ];
 
-console.log(pizzas);
+console.log(pizzas.length);
+console.log(pizzas[0].name);
 
-console.log(image);
+pizzas.forEach(pizzaLoop);
+
+// Adds a row for 1 kind of pizza
+function pizzaLoop(pizza) {
+    // console.log(pizza);
+    var row = document.createElement("TR");
+    var info = document.createElement("TD");
+    var info1 = document.createElement("TD");
+    var info2 = document.createElement("TD");
+    var title = document.createTextNode(pizza.name);
+    // var title1 = document.createTextNode(pizzas[0].name);
+    var description = document.createTextNode(pizza.ingredients);
+    var price = document.createTextNode(pizza.price);
+    row.appendChild(info);
+    row.appendChild(info1);
+    row.appendChild(info2);
+    info.appendChild(title);
+    info1.appendChild(description);
+    info2.appendChild(price);
+    table.appendChild(row);
+    console.log(info1);
+}
         
